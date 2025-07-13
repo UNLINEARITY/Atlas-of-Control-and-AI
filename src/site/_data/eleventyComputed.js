ee9b9f7e865d6dac9c28df1c929df615b51077f5
unlinearity <134848055+UNLINEARITY@users.noreply.github.com>
2025-07-14 00:28:13 +0800

    Update eleventyComputed.js

--- src/site/_data/eleventyComputed.js
+++ src/site/_data/eleventyComputed.js
@@ -2,10 +2,11 @@ const { getGraph } = require("../../helpers/linkUtils");
 const { getFileTree } = require("../../helpers/filetreeUtils");
 const { userComputed } = require("../../helpers/userUtils");
 
+
 module.exports = async function (data) {
   return {
     graph: await getGraph(data),
-    filetree: await getFileTree(data),         // 仅当 getFileTree 也是 async
-    userComputed: await userComputed(data)     // 同理，若非 async 可省略 await
+    filetree: await getFileTree(data),        
+    userComputed: await userComputed(data)     
   };
 };
