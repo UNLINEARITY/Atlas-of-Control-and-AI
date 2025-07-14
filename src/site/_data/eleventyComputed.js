a57d8994e8ed0b6c1a7b673051da322818a68302
unlinearity <134848055+UNLINEARITY@users.noreply.github.com>
2025-07-14 09:31:18 +0800

    Revert "Conflict resolution"

--- src/site/_data/eleventyComputed.js
+++ src/site/_data/eleventyComputed.js
@@ -2,11 +2,8 @@ const { getGraph } = require("../../helpers/linkUtils");
 const { getFileTree } = require("../../helpers/filetreeUtils");
 const { userComputed } = require("../../helpers/userUtils");
 
-
-module.exports = async function (data) {
-  return {
-    graph: await getGraph(data),
-    filetree: await getFileTree(data),        
-    userComputed: await userComputed(data)     
-  };
+module.exports = {
+  graph: (data) => getGraph(data),
+  filetree: (data) => getFileTree(data),
+  userComputed: (data) => userComputed(data)
 };
